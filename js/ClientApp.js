05b3b1d5ff94fe399dfea1ad2a1cd98104763bd5
Fortune Ekeruo <legalmody@gmail.com>
2017-11-01 12:54:39 +0100

    Format code with prettier

--- js/ClientApp.js
+++ js/ClientApp.js
@@ -2,25 +2,25 @@
 
 var ce = React.createElement;
 
-var MyTitle = function (props) {
-    return (
-        ce('div', null, 
-            ce('h1', {style: {color: props.color}}, props.title)
-        )
-    );
+var MyTitle = function(props) {
+  return ce(
+    "div",
+    null,
+    ce("h1", { style: { color: props.color } }, props.title)
+  );
 };
 
 var MyFirstComponent = function() {
-    return (
-        ce('div', null, 
-            ce(MyTitle, {title: 'House of Cards', color: 'rebeccapurple'}),
-            ce(MyTitle, {title: 'Orange is the New Black', color: 'peru'}),
-            ce(MyTitle, {title: 'Stranger Things', color: 'burlywood'})
-        )
-    );
+  return ce(
+    "div",
+    null,
+    ce(MyTitle, { title: "House of Cards", color: "rebeccapurple" }),
+    ce(MyTitle, { title: "Orange is the New Black", color: "peru" }),
+    ce(MyTitle, { title: "Stranger Things", color: "burlywood" })
+  );
 };
 
 ReactDOM.render(
-    React.createElement(MyFirstComponent),
-    document.getElementById("app")
+  React.createElement(MyFirstComponent),
+  document.getElementById("app")
 );
